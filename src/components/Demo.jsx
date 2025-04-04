a32850991d05bbf71c84a9f92195617408dbaac9
Ziyad Hammad <ziyadhammad0@gmail.com>
2025-04-04 19:00:27 -0400

    update

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -111,7 +111,7 @@ const Demo = () => {
         ) : (
           <div className="flex flex-col gap-3">
             <h2 className="font-santoshi font-bold text-gray-600 text-xl">
-              Article <span className="blue_gradient">Summary</span>
+              Article <span className="orange_gradient">Summary</span>
             </h2>
             <div className="summary_box">
               <p className="font-inter font-medium text-sm text-gray-700">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ const Hero = () => {
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="w-full flex justify-between items-center flex-row mb-10 pt-3">
         <h2 className="w-28 object-contain font-bold text-4xl">
-          Rapid<span className="blue_gradient">Sum</span>
+          Rapid<span className="orange_gradient">Sum</span>
         </h2>
 
         <button
@@ -19,7 +19,7 @@ const Hero = () => {
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="blue_gradient">Open AI GPT-4</span>
+        <span className="orange_gradient">Open AI GPT-4</span>
       </h1>
       <h2 className="desc">
         Streamline your reading experience with RapidSum – designed to condense
